b40d7bc9943a2dc50b7a694d89c20505b44e5031
Stefano Della Morte <stefano1996linc@gmail.com>
2021-01-20 18:15:03 +0000

    Notebook completed

--- code/GO.py
+++ code/GO.py
@@ -1,5 +1,5 @@
 #Generator for UniProt entries
-from lmxl import etree
+from lxml import etree
 
 def get_entries(f):
     entry = ''
@@ -21,3 +21,78 @@ def parse_xml_entry(xml):
         if dbReference.get("type") == "GO":
             annotations.append(dbReference.get("id"))
     return accession, annotations
+
+
+def parse_obo(obo_file):
+    # Parse the ontology, exclude obsolete terms
+    graph = {}  # { term_id : term_object }
+    obj = {}  # { id: term_id, name: definition, is_a: list_of_parents, is_obsolete: True, namespace: namespace }
+    with open(obo_file) as f:
+        for line in f:
+            line = line.strip().split(": ")
+            if line and len(line) > 1:
+                # print(line)
+                k, v = line[:2]
+                if k == "id" and v.startswith("GO:"):
+                    obj["id"] = v
+                elif k == "name":
+                    obj["def"] = v
+                elif k == "is_a" and v.startswith("GO:"):
+                    obj.setdefault("is_a", []).append(v.split()[0])
+                elif k == "is_obsolete":
+                    obj["is_obsolete"] = True
+                elif k == "namespace":
+                    obj["namespace"] = v
+            else:
+                if obj.get("id") and not obj.get("is_obsolete"):
+                    if "is_a" not in obj:
+                        obj["is_root"] = True
+                    graph[obj["id"]] = obj
+                    # print(obj)
+                obj = {}
+    return graph
+
+
+def get_ancestors(graph):
+    """
+    Build a dictionary of ancestors
+    and calculate term depth (shortest path)
+    """
+
+    roots = set()
+    for node in graph:
+        if graph[node].get("is_root"):
+            roots.add(node)
+
+    depth = {}
+    ancestors = {}  # { term : list_of_ancestor_terms }
+    for node in graph:
+        c = 0
+        node_ancestors = []
+        node_parents = graph[node].get("is_a")
+
+        # Loop parents levels (parents of parents) until no more parents
+        while node_parents:
+            c += 1
+
+            # Set root
+            if node not in depth and roots.intersection(set(node_parents)):
+                depth[node] = c
+
+            # Add ancestors
+            node_ancestors.extend(node_parents)
+
+            # Update the list of parents (1 level up)
+            node_parents = [term for parent in node_parents for term in graph[parent].get("is_a", [])]
+
+        ancestors[node] = set(node_ancestors)
+    return ancestors, depth, roots
+
+
+def get_children(ancestors):
+    children = {}  # { node : list_of_children }, leaf terms are not keys
+    for node in ancestors:
+        for ancestor in ancestors[node]:
+            children.setdefault(ancestor, set()).add(node)
+    return children
+
--- code/setup.sh
+++ code/setup.sh
@@ -1,10 +1,8 @@
-# Install blast (linux build 64bit)
 echo "Installing BLAST..."
 wget -P ../binx/ ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.11.0+-x64-linux.tar.gz
 tar -xf ../binx/ncbi-blast-2.11.0+-x64-linux.tar.gz -C ../binx/
 rm ../binx/ncbi-blast-2.11.0+-x64-linux.tar.gz
 
-# Download SwissProt sequence
 echo "Downloading SwissProt DB..."
 wget -P ../data/db/ ftp://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
 gunzip ../data/db/uniprot_sprot.fasta.gz
@@ -13,37 +11,34 @@ rm ../data/db/uniprot_sprot.fasta.gz
 # Create database (Blast indexes, phr+pin+psq files)
 ../binx/ncbi-blast-2.11.0+/bin/makeblastdb -dbtype prot -in ../data/db/uniprot_sprot.fasta
 
-# Download and compile HMMER
 echo "Installing HMMER..."
 wget -P ../binx/ http://eddylab.org/software/hmmer/hmmer-3.3.1.tar.gz
 tar -xf ../binx/hmmer-3.3.1.tar.gz -C ../binx/
 rm ../binx/hmmer-3.3.1.tar.gz
 cd ../binx/hmmer-3.3.1/ && ./configure && make
 
-# Get T-Coffee Python Wrapper
 echo "Getting T-Coffee wrapper..."
 wget ../code/ https://raw.githubusercontent.com/ebi-wp/webservice-clients/master/python/tcoffee.py
 
- Get PDB database
 echo "Getting PDBs..."
 wget -P ../data/ ftp://ftp.ebi.ac.uk/pub/databases/msd/sifts/flatfiles/csv/uniprot_pdb.csv.gz
 tar -xf ../data/uniprot_pdb.csv.gz ../data/
 rm ../data/uniprot_pdb.csv.gz
 
-# Download TM-Align
 echo "Installing TM-Align..."
 wget -P ../binx/ https://zhanglab.ccmb.med.umich.edu/TM-align/TMalign.gz
 gunzip ../binx/TMalign.gz
 rm ../binx/TMalign.gz
 sudo chmod 777 ../binx/TMalign
 
-# Get CATH files...
 echo "Getting CATH DB..."
 wget -P ../data/ ftp://orengoftp.biochem.ucl.ac.uk/cath/releases/daily-release/newest/cath-b-newest-all.gz
 wget -P ../data/ ftp://ftp.ebi.ac.uk/pub/databases/msd/sifts/flatfiles/csv/pdb_chain_cath_uniprot.csv.gz
 
-# Get SwissProt
 echo "Getting SwissProt DB .xml file..."
 wget -P ../data/ ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.xml.gz
+
+echo "Getting gene ontology..."
+wget -P ../data/ http://purl.obolibrary.org/obo/go.obo
 
 echo "Done."
